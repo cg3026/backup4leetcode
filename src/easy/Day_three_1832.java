df7b89bd92ff8206154e642811f297180f4bcc86
cg3026 <cg3026@126.com>
2022-11-02 22:27:48 +0800

    D3

---
+++ src/easy/Day_three_1832.java
@@ -0,0 +1,43 @@
+package easy;
+
+public class Day_three_1832 {
+
+    // 大内存占用
+    public boolean checkIfPangram_bm(String sentence){
+        boolean flag = true;
+        int[] count = new int[26];
+        int len = sentence.length();
+        for (int l=0; l < len; l++){
+            count[sentence.charAt(l)-'a']++;
+        }
+        for(int letter:count){
+            if (letter == 0) {
+                flag = false;
+                break;
+            }
+        }
+        return flag;
+    }
+    public boolean checkIfPangram_lowm(String sentence) {
+        boolean[] cache = new boolean[26];
+        char[] ss = sentence.toCharArray();
+        for (char c : ss) cache[c - 'a'] = true;
+        for (boolean b : cache) if (!b) return false;
+        return true;
+    }
+
+    public boolean checkIfPangram_fast(String sentence){
+        for (int l=97; l < 123; l++){
+            if(sentence.indexOf((char)l) == -1){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        String str = "thequickbrownfoxjumpsoverthelazydog";
+        Day_three_1832 dt = new Day_three_1832();
+        boolean flagg = dt.checkIfPangram_fast(str);
+        System.out.println(flagg);
+    }
+}
---
+++ src/medium/Day_three_1620.java
@@ -0,0 +1,53 @@
+package medium;
+
+
+public class Day_three_1620 {
+
+    public double o_distance(int[] coordinate_0, int[] coordinate_1){
+        return Math.sqrt(Math.pow((coordinate_0[0]-coordinate_1[0]), 2) + Math.pow((coordinate_0[1]-coordinate_1[1]), 2));
+    }
+    public int[] radius_range(int[][] towers, int radius){
+        int[] range = new int[4];
+        for (int[] tower: towers){
+            range[0] = Math.min(range[0], Math.max(tower[0]- radius, 0) );
+            range[1] = Math.max(range[1], tower[0] + radius);
+            range[2] = Math.min(range[2], Math.max(tower[1] - radius, 0));
+            range[3] = Math.max(range[3], tower[1] + radius);
+        }
+        return range;
+    }
+    // 暴力破解，嘎嘎慢
+    public int[] bestCoordinate(int[][] towers, int radius) {
+        int[] range = radius_range(towers, radius);
+        int[] point = new int[2];
+        int max_radius = 0;
+        for (int i=0;i<range[1]-range[0]+1;i++){
+            for (int j=0;j<range[3]-range[2]+1;j++){
+                int radius_m = 0;
+                for(int[] tower:towers){
+                    double d =o_distance(new int[]{i + range[0], j + range[2]}, new int[]{tower[0],tower[1]});
+                    if (d <= radius) {
+                        radius_m = radius_m + (int)(tower[2]/(1+d));
+                    }
+                }
+                if (radius_m > max_radius){
+                    max_radius = radius_m;
+                    point[0] = i + range[0];
+                    point[1] = j + range[2];
+                }
+            }
+        }
+        return point;
+    }
+
+    public static void main(String[] args) {
+        int[] co_1 = new int[]{0,0,0};
+        int[] co_2 = new int[]{1,1,1};
+        Day_three_1620 dt = new Day_three_1620();
+        int [][] towers = new int[][]{{1,2,5},{2,1,7},{3,1,9}};
+        int radius = 2;
+        int[] pp = dt.bestCoordinate(towers, radius);
+        System.out.println(pp[0]);
+        System.out.println(pp[1]);
+    }
+}
